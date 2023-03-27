1cf23d1d9ce2c76eb982854cd37cad3ad4f48e66
Corvax <vlvete1990@yandex.ru>
2023-03-27 22:31:35 +0300

    chore: added handlerClick

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-console.log("Hello world!");
+function handlerClick() {
+  const element = document.getElementById("header");
+  element.innerHTML = "Hello world!";
 
-function heandlerClick() {
-  document.getElementById("header").innerHTML = "Hello world!"
-
+  const list = ['Apples', 'Pears', 'Orange'];
+  const listElement = document.getElementById('list');
+  list.forEach(function(fruit) {
+    const el = document.createElement('li');
+    el.innerHTML = fruit;
+    listElement.appendChild(el);
+  })
 }
