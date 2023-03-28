4a6ff95da74827159fed47dff75f38df644f960b
Corvax <vlvete1990@yandex.ru>
2023-03-28 19:43:55 +0300

    chore: added 3 css loader folder

---
+++ 3. CSS Loaders/src/index.js
@@ -0,0 +1,15 @@
+import './index.css';
+document.getElementById('btn1').addEventListener('click', handlerClick)
+
+function handlerClick() {
+  const element = document.getElementById("header");
+  element.innerHTML = "Hello world!";
+
+  const list = ['Apples', 'Pears', 'Orange'];
+  const listElement = document.getElementById('list');
+  list.forEach(function(fruit) {
+    const el = document.createElement('li');
+    el.innerHTML = fruit;
+    listElement.appendChild(el);
+  })
+}
