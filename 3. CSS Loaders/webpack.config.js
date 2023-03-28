7a8e5a9237b9dde4169ff440c0dc35ce57ff3890
Corvax <vlvete1990@yandex.ru>
2023-03-28 19:48:19 +0300

    chore: added style-loader css-loader

--- 3. CSS Loaders/webpack.config.js
+++ 3. CSS Loaders/webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     filename: 'bundler.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
 }
