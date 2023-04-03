f3cbb3120fd2b417efca4e370e7a9d8e0a3f2457
Corvax <vlvete1990@yandex.ru>
2023-04-03 16:24:25 +0300

    chore: added Font Loaders

---
+++ 6. Font Loaders/src/clearButton.js
@@ -0,0 +1,11 @@
+import style from './clearButton.scss'
+
+const el = document.createElement('button');
+el.innerHTML = "Clear button";
+el.classList.add([style.button]);
+
+el.onclick = function() {
+  alert("Clear button clicked");
+}
+
+document.body.appendChild(el);
---
+++ 6. Font Loaders/src/index.js
@@ -0,0 +1,25 @@
+import style from './index.scss';
+import './clearButton';
+import logo from './assets/logo.png';
+import './assets/fonts/Redressed-Regular.ttf'
+
+const btn1 = document.getElementById('btn1');
+const logEl = document.getElementById('logo');
+
+btn1.addEventListener('click', function() {
+  const element = document.getElementById("header");
+  element.innerHTML = "Hello world!";
+  element.classList.add([style.header]);
+
+  const list = ['Apples', 'Pears', 'Orange'];
+  const listElement = document.getElementById('list');
+
+  list.forEach(function(fruit) {
+    const el = document.createElement('li');
+    el.innerHTML = fruit;
+    listElement.appendChild(el);
+  })
+})
+
+btn1.classList.add([style.button]);
+logEl.src = logo;
