d95c4fb4d7c37513c22bf75883eedf8044b99e1f
Corvax <vlvete1990@yandex.ru>
2023-04-08 17:06:47 +0300

    chore: copy plugin

---
+++ 8.4. Course Project_CopyPlugin/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin"); 
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    courses: "./src/pages/courses.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        type: "asset/resource"
+      }
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      chunks: ["index"],
+      filename: "index.html"
+    }),
+    new HTMLWebpackPlugin({
+      template: "./src/pages/courses.html",
+      chunks: ["courses"],
+      filename: "courses.html"
+    })
+  ]
+};
