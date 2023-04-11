41e7ed1e6a1c56de8fbd687e70127737ea2083f0
Corvax <vlvete1990@yandex.ru>
2023-04-11 22:59:54 +0300

    chore: added imports sourse

--- 8.4. Course Project_CopyPlugin/src/pages/courses.js
+++ 8.4. Course Project_CopyPlugin/src/pages/courses.js
@@ -1,4 +1,15 @@
-// import courseData from "../data/courses.json";
+import bootstrap from 'bootstrap';
+import $ from 'jquery';
+import Typed from 'typed.js';
+import validate from 'validate.js';
+import toastr from 'toastr';
+import _ from 'lodash';
+
+import "../index.css";
+import "../__vendor.scss";
+import "../assets/css/font-awesome.min.css"
+
+import courseData from "../data/courses.json";
 
 function loadCourses() {
   const courseListSection = $("#course-list")[0];
