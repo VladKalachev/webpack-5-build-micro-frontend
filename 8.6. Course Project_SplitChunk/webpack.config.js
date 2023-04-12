a8865323b672dfddd934cb2dffcb83b5b497b0db
Corvax <vlvete1990@yandex.ru>
2023-04-12 22:15:54 +0300

    chore: added Split Chunk

---
+++ 8.6. Course Project_SplitChunk/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    courses: "./src/pages/courses.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        type: "asset/resource"
+      }
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      chunks: ["index"],
+      filename: "index.html"
+    }),
+    new HTMLWebpackPlugin({
+      template: "./src/pages/courses.html",
+      chunks: ["courses"],
+      filename: "courses.html"
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets/images/"),
+          to: path.resolve(__dirname, "dist"),
+          context: "src",
+        }        
+      ],
+    }),
+    new BundleAnalyzerPlugin({
+
+    })
+  ]
+};
